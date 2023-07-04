7e73e6f949ae7884794d86728ec39f43e82d0633
jocke-champ <jockeeee.s@gmail.com>
2023-07-04 19:21:21 +0200

    v2 04-07-2023

--- backend/models.go
+++ backend/models.go
@@ -9,6 +9,7 @@ type User struct {
 	gorm.Model
 	Email        string     `gorm:"type:varchar(100);unique_index" json:"email"`
 	PasswordHash string     `json:"password"`
+	Name         string     `json:"name"`
 	Posts        []Post     `gorm:"foreignkey:UserID"`
 	Comments     []Comment  `gorm:"foreignkey:UserID"`
 	Locations    []Location `gorm:"foreignkey:UserID"`
